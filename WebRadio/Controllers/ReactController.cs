bde0f5d4aebf18176927c8c675f37c45c9531b89
Mario Andres Botero Florez <mariobot113@hotmail.com>
2018-10-12 14:06:19 -0500

    Add React JC Functionality example

---
+++ WebRadio/Controllers/ReactController.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using System.Web.UI;
+using WebRadio.Models;
+
+namespace WebRadio.Controllers
+{
+    public class ReactController : Controller
+    {
+        private static readonly IList<CommentModel> _comments;
+
+        static ReactController() {
+            _comments = new List<CommentModel>
+            {
+                new CommentModel
+                {
+                    Id = 1,
+                    Author = "Daniel Lo Nigro",
+                    Text = "Hello ReactJS.NET World!"
+                },
+                new CommentModel
+                {
+                    Id = 2,
+                    Author = "Pete Hunt",
+                    Text = "This is one comment"
+                },
+                new CommentModel
+                {
+                    Id = 3,
+                    Author = "Jordan Walke",
+                    Text = "This is *another* comment"
+                },
+            };
+        }
+
+        // GET: React
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        [OutputCache(Location = OutputCacheLocation.None)]
+        public ActionResult Comments()
+        {
+            return Json(_comments, JsonRequestBehavior.AllowGet);
+        }
+
+        [HttpPost]
+        public ActionResult AddComment(CommentModel comment)
+        {
+            // Create a fake ID for this comment
+            comment.Id = _comments.Count + 1;
+            _comments.Add(comment);
+            return Content("Success :)");
+        }
+
+
+    }
+}
